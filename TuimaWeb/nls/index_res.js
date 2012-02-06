c8c7270867b634f31477b1926394da1e12c5f3e6
unknown <mikko@.(none)>
2012-02-06 23:23:40 +0200

    added dojo form and button

--- TuimaWeb/nls/index_res.js
+++ TuimaWeb/nls/index_res.js
@@ -2,7 +2,8 @@ define({
     root: ({
 		PAGE_TITLE:"Archive to your recipes",
 		GREETING: "Archive your favourite recipes to your own personal archive. Seek them later easily.",
-		COPY_PASTE_AREA: "Copy/Paste recipe here:"
+		COPY_PASTE_AREA: "Copy/Paste recipe here:",
+		PASTE_SUBMIT: "Save"
     }),
     "fi": true
 });
