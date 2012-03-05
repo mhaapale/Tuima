7338c84b4f53dc718393f98d58de68f2633e271f
Mikko Haapalehto <mikko.haapalehto@gmail.com>
2012-03-05 23:41:07 +0200

    new search page

--- TuimaWeb/nls/index_res.js
+++ TuimaWeb/nls/index_res.js
@@ -3,7 +3,8 @@ define({
 		PAGE_TITLE:"Archive to your recipes",
 		GREETING: "Archive your favourite recipes to your own personal archive. Seek them later easily.",
 		COPY_PASTE_AREA: "Copy/Paste recipe here:",
-		PASTE_SUBMIT: "Save"
+		PASTE_SUBMIT: "Save",
+		SEARCH_SUBMIT: "Search"
     }),
     "fi": true
 });
